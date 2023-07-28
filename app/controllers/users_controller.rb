28bb1198d24440d7408e5f47d5bfc7c5e1e4f22b
ljhxksy <anh.hoang649053@tufts.edu>
2023-07-28 17:14:56 +0700

    Chapter-10

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,29 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :index, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted!"
+    redirect_to users_url
+  end
+
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "Please log in"
+      redirect_to login_url
+    end
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
+  end
 
   def show
     @user = User.find(params[:id])
@@ -13,7 +38,7 @@ class UsersController < ApplicationController
     if @user.save
       log_in @user
       flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+      redirect_back_or @user
     else
       render 'new'
     end
@@ -21,7 +46,16 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+  end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Profile updated!"
+      redirect_to @user
+    else
+      render :edit, status: 422
+    end
   end
 
   private
@@ -31,4 +65,7 @@ class UsersController < ApplicationController
                                  :password_confirmation)
   end
 
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
+  end
 end
